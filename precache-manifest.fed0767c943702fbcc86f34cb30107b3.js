5e787432dd3e75941dc496776e28cdabeb150ba2
kochatkov <vlad.kochatkov@gmail.com>
2020-05-07 16:50:52 +0300

    Updates

---
+++ precache-manifest.fed0767c943702fbcc86f34cb30107b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f346f8b4c474315610b78749b363b1ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7614b4c06fdaf21ddb6b",
+    "url": "./static/css/main.eddf792a.chunk.css"
+  },
+  {
+    "revision": "95e2ac7a91498eac1241",
+    "url": "./static/js/2.49182981.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.49182981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7614b4c06fdaf21ddb6b",
+    "url": "./static/js/main.a96faf3d.chunk.js"
+  },
+  {
+    "revision": "548aac5e4720dc7e1ea8",
+    "url": "./static/js/runtime-main.9c94e0be.js"
+  }
+]);
